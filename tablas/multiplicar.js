c3d15bcbb799fa9d6a8abfbb3eba074916e31313
FabricioQs <iefc.kevin.quishpe@gmail.com>
2026-07-09 19:08:53 -0500

    Parte 7 y 8 completadas: Caja de texto agregada y tabla dinámica funcional

--- tablas/multiplicar.js
+++ tablas/multiplicar.js
@@ -1,10 +1,11 @@
 function generarTablas() {
     let contenedor = document.getElementById("divTabla");
-    let contenido ="";
-
-    for (let i = 1; i<=10; i++ ){
-        let resultado = 3 * 1;
-         contenido = contenido + "<div class='fila-multiplicar'>3 x " + i + " = " + resultado + "</div>";
+    let numeroTabla = parseInt(document.getElementById("txtTabla").value);
+    let contenido = "";
+    
+    for (let i = 1; i <= 10; i++) {
+        let resultado = numeroTabla * i;
+        contenido = contenido + "<div class='fila-multiplicar'>" + numeroTabla + " x " + i + " = " + resultado + "</div>";
     }
     contenedor.innerHTML = contenido;
 }
